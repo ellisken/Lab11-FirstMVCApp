e52e17048a800fe565406e47919e7a1c8be38d95
ellisken <kse104@gmail.com>
2018-10-23 10:42:13 -0700

    displaying results within year range

--- PersonOfTheYear/PersonOfTheYear/Controllers/HomeController.cs
+++ PersonOfTheYear/PersonOfTheYear/Controllers/HomeController.cs
@@ -28,7 +28,7 @@ namespace PersonOfTheYear.Controllers
         {
             //Get List of persons
             List<Person> persons = Person.GetPersons(startYear, endYear);
-            return View(persons[0]);
+            return View(persons);
         }
 
 
